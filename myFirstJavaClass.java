2a56f5ce21fed257e83b5e7c6451ba2aaf55aec2
genxhero <genxhero@users.noreply.github.com>
2020-03-15 10:33:40 -0700

    hmm

--- myFirstJavaClass.java
+++ myFirstJavaClass.java
@@ -1,6 +1,8 @@
 public class myFirstJavaClass {
-    public static void main (String arg) {
-        String output = String.format("Hello, %s!", name);
+
+    public static void main () {
+        String name = "Ass";
+        String output = String.format("Hello, %s", name);
         System.out.println(output);
     }
 
