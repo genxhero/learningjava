d2ea80809fc45caec067ed2f3ee6ca8532521825
genxhero <genxhero@users.noreply.github.com>
2020-03-15 10:12:23 -0700

    call ab from main

--- myFirstJavaClass.java
+++ myFirstJavaClass.java
@@ -1,5 +1,10 @@
 public class myFirstJavaClass {
-    public static void main (String []args) {
-        System.out.println("Hello World");
+    public static void main (String arg) {
+        assbutt(arg);
+    }
+
+    public static void assbutt (String name) {
+        String output = String.format("Hello, %s!", name);
+        System.out.println(output);
     }
 }
