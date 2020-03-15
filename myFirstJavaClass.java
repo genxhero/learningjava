035b7cb40957382f9ff42966c98814e01c8e19a5
genxhero <genxhero@users.noreply.github.com>
2020-03-15 10:13:21 -0700

    hmm

--- myFirstJavaClass.java
+++ myFirstJavaClass.java
@@ -3,7 +3,7 @@ public class myFirstJavaClass {
         assbutt(arg);
     }
 
-    public static void assbutt (String name) {
+    public static void greeting (String name) {
         String output = String.format("Hello, %s!", name);
         System.out.println(output);
     }
