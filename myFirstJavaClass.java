d57ffdf503bc5dbffae202562836a91b657e599b
genxhero <genxhero@users.noreply.github.com>
2020-03-15 10:16:23 -0700

    move stuff to main for now

--- myFirstJavaClass.java
+++ myFirstJavaClass.java
@@ -1,10 +1,7 @@
 public class myFirstJavaClass {
     public static void main (String arg) {
-        assbutt(arg);
-    }
-
-    public static void greeting (String name) {
         String output = String.format("Hello, %s!", name);
         System.out.println(output);
     }
+
 }
