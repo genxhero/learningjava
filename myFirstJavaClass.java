9f53e0a7b4299ab0567691854ed39becf7b436a1
genxhero <genxhero@users.noreply.github.com>
2020-03-16 10:22:12 -0700

    remove first class

--- myFirstJavaClass.java
+++
@@ -1,8 +0,0 @@
-public class myFirstJavaClass {
-
-    public static void main (String name) {
-        String output = String.format("Hello, %s", name);
-        System.out.println(output);
-    }
-
-}
