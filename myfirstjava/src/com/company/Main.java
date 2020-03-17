70c8059e3722982ced7872509432f38577c62634
genxhero <genxhero@users.noreply.github.com>
2020-03-17 16:40:28 -0700

    change default name

--- myfirstjava/src/com/company/Main.java
+++ myfirstjava/src/com/company/Main.java
@@ -3,7 +3,7 @@ package com.company;
 public class Main {
 
     public static void main(String[] args) {
-//	Greeter.sayName("Asshole");
+//	Greeter.sayName("Dragon");
         System.out.println("grr");
     }
 }
