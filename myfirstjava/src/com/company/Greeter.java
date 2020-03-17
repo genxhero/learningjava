9edc3473cd6e0eb8e99c886eb936d370adef5df1
genxhero <genxhero@users.noreply.github.com>
2020-03-17 16:43:34 -0700

    clean up code a bit

--- myfirstjava/src/com/company/Greeter.java
+++ myfirstjava/src/com/company/Greeter.java
@@ -1,8 +1,8 @@
-//package com.company;
-//
-//public class Greeter {
-//    public static void sayName (String name) {
-//        String greeting = String.format("Hello, %s", name);
-//        System.out.println(greeting);
-//    }
-//}
+package com.company;
+
+public class Greeter {
+    public static void sayName (String name) {
+        String greeting = String.format("Hello, %s", name);
+        System.out.println(greeting);
+    }
+}
--- myfirstjava/src/com/company/Main.java
+++ myfirstjava/src/com/company/Main.java
@@ -1,9 +1,8 @@
 package com.company;
 
 public class Main {
-
     public static void main(String[] args) {
-//	Greeter.sayName("Dragon");
+//	    Greeter.sayName("Dragon");
         System.out.println("grr");
     }
 }
