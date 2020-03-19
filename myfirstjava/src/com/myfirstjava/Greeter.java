d8a926e5295756d73096d0b0d3dfcca1ee5e61c6
genxhero <genxhero@users.noreply.github.com>
2020-03-19 16:17:45 -0700

    delete useless files

--- myfirstjava/src/com/myfirstjava/Greeter.java
+++
@@ -1,8 +0,0 @@
-package com.company;
-
-public class Greeter {
-    public static void sayName (String name) {
-        String greeting = String.format("Hello, %s", name);
-        System.out.println(greeting);
-    }
-}
--- myfirstjava/src/com/myfirstjava/Main.java
+++
@@ -1,8 +0,0 @@
-package myfirstjava;
-
-public class Main {
-    public static void main(String[] args) {
-//	    Greeter.sayName("Dragon");
-        System.out.println("grr");
-    }
-}
